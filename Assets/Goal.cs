03bb5389a696bbddce4d649b80d53f054ef4a606
Xlib <aman9893089064@gmail.com>
2023-02-15 02:23:01 +0530

    Added End game and Game Won systems

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class Goal : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.CompareTag("Ground"))
+        if(collider.CompareTag("Player"))
         {
+            Debug.Log("Game Won");
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
