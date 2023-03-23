80dba99d37df6ffcd54a78cd79d948345faa8847
Xlib <aman9893089064@gmail.com>
2023-03-23 23:52:23 +0530

    Completed Main Menu & Started Shop Menu

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+
+public class SceneManagement : MonoBehaviour
+{
+    public string sceneName;
+
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
