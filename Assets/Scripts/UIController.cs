722003cd71e53fe1cfe26208895c78dd7f4dbdb5
Xlib <aman9893089064@gmail.com>
2023-03-26 00:06:51 +0530

    dummy

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    public void UpgradeSuspension()
+    {
+
+    }
+
+    public void UpgradeEngine()
+    {
+
+    }
+
+    public void UpgradeChasis()
+    {
+
+    }
+
+    public void UpgradeTyres()
+    {
+
+    }
+}
