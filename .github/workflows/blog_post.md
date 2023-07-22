8bfdb9575ba483a0d124ff342638ab9187e9e8a0
Xlib <aman9893089064@gmail.com>
2023-07-22 22:42:50 +0530

    added blog_post py and md

---
+++ .github/workflows/blog_post.md
@@ -0,0 +1,10 @@
+---
+title: My First Blog Post
+date: 2023-07-23
+---
+
+This is my first blog post!
+
+I'm excited to start blogging and sharing my thoughts on technology.
+
+I hope you enjoy reading my blog posts!
---
+++ .github/workflows/generate_blog_post.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+def generate_blog_post():
+  """Generates a blog post from a file."""
+
+  # Get the path to the blog post file.
+  blog_post_file = os.path.join(os.getcwd(), "blog_post.md")
+
+  # Read the content of the blog post file.
+  with open(blog_post_file) as f:
+    blog_post_content = f.read()
+
+  # Generate the HTML content of the blog post.
+  html_content = markdown.markdown(blog_post_content)
+
+  # Write the HTML content of the blog post to a file.
+  with open("blog_post.html", "w") as f:
+    f.write(html_content)
+
+if __name__ == "__main__":
+  generate_blog_post()
