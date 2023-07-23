232cc5de351f99bbbcaec11c16aeb9ce358ccf42
Xlib <aman9893089064@gmail.com>
2023-07-23 18:54:24 +0530

    updated stuff

--- .github/workflows/generate_blog_post.py
+++ .github/workflows/generate_blog_post.py
@@ -1,30 +1,22 @@
 import git
 import markdown2
 
-# Initialize the Git repository
-repo = git.Repo()
+# Function to convert Markdown to HTML
+def convert_to_html(markdown_text):
+    return markdown2.markdown(markdown_text)
 
-# Get the list of commits
-commits = list(repo.iter_commits('main'))
+# Get the commit message for the latest commit
+repo = git.Repo(search_parent_directories=True)
+commit = next(repo.iter_commits())
 
-# Initialize the Markdown converter
-markdowner = markdown2.Markdown()
+# Create the blog post content with commit message
+blog_post_content = f"<h2>Commit {commit.hexsha}</h2>\n\n<p>{commit.message}</p>"
 
-# Loop through each commit
-for commit in commits:
-    # Extract commit message
-    commit_message = commit.message
+# Convert the blog post content to HTML
+html_content = convert_to_html(blog_post_content)
 
-    # Get the code diff for the commit
-    code_diff = commit.diff(commit.parents[0]).__str__()
+# Save the HTML content to blog_post.html
+with open("blog_post.html", "w", encoding="utf-8") as file:
+    file.write(html_content)
 
-    # Convert the code diff to HTML
-    pandoc_html = markdowner.convert(code_diff)
-
-    # Add commit message to the beginning of the HTML content
-    commit_html = f"<h2>Commit Message:</h2><p>{commit_message}</p>"
-    pandoc_html_with_commit = commit_html + pandoc_html
-
-    # Write the HTML content with the commit message to blog_post.html
-    with open("blog_post.html", "w") as html_file:
-        html_file.write(pandoc_html_with_commit)
+print("Blog post generated successfully.")
