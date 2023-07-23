fce9ddaccf2e80c6ffff3d0e7e938d3e30ee8a93
Xlib <aman9893089064@gmail.com>
2023-07-23 17:15:19 +0530

    Update generate_blogs.py

--- generate_blogs.py
+++ generate_blogs.py
@@ -1,43 +1,29 @@
-# generate_blogs.py
-
-import os
 import git
 import markdown2
 
-# Define the output directory for the blog posts
-output_directory = "blogs"
+# Initialize the repository
+repo = git.Repo(".")
 
-def generate_blog_post(commit_hash, commit_message, code_diff):
-    # Create a blog post file for each commit
-    blog_filename = f"{output_directory}/{commit_hash}.md"
+# Create a list to store the commit descriptions
+commit_descriptions = []
 
-    # Compose the blog content with commit message and code changes
-    blog_content = f"# Blog Post for Commit: {commit_hash}\n\n"
-    blog_content += f"## Commit Message:\n\n{commit_message}\n\n"
-    blog_content += "## Code Changes:\n\n```diff\n"
-    blog_content += code_diff
-    blog_content += "\n```"
+# Loop through all commits and fetch their descriptions
+for commit in repo.iter_commits():
+    commit_descriptions.append(commit.message.strip())
 
-    # Convert Markdown to HTML
-    blog_html = markdown2.markdown(blog_content)
+# Create the blog content
+blog_content = "# My Blog\n\n"
 
-    # Write the HTML content to the blog post file
-    with open(blog_filename, "w", encoding="utf-8") as f:
-        f.write(blog_html)
+# Append each commit description as a separate section in the blog
+for index, commit_desc in enumerate(commit_descriptions, 1):
+    blog_content += f"## Commit {index}\n\n"
+    blog_content += f"{commit_desc}\n\n"
 
-if __name__ == "__main__":
-    # Create the output directory if it doesn't exist
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
+# Convert the blog content to HTML
+html_content = markdown2.markdown(blog_content)
 
-    # Initialize the Git repository
-    repo = git.Repo(".")
-    commits = list(repo.iter_commits())
+# Save the HTML content to the blog_post.html file
+with open("blog_post.html", "w", encoding="utf-8") as html_file:
+    html_file.write(html_content)
 
-    # Iterate through the commits and generate blog posts
-    for commit in commits:
-        commit_hash = commit.hexsha
-        commit_message = commit.message.strip()
-        code_diff = commit.diff(commit.parents[0]).__str__()
-
-        generate_blog_post(commit_hash, commit_message, code_diff)
+print("Blog post generated successfully.")
