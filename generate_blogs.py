d790b2a6c9162a717a85812d72bc85c2128124cd
Xlib <aman9893089064@gmail.com>
2023-07-24 00:12:14 +0530

    Update generate_blogs.py

--- generate_blogs.py
+++ generate_blogs.py
@@ -1,19 +1,19 @@
 import os
 from git import Repo
+import markdown2
 
 # Function to get the commit messages and generate HTML blog post content
 def generate_blog_post(repo_path):
     repo = Repo(repo_path)
     commits = repo.iter_commits()
     
-    blog_content = ""
+    blog_content = "# My Blog\n\n"
     
     for commit in commits:
         commit_sha = commit.hexsha
         commit_message = commit.message.strip()
-        commit_html = f"<h2>Commit {commit_sha}</h2><p>{commit_message}</p>"
-        
-        blog_content += commit_html
+        blog_content += f"## Commit {commit_sha}\n\n"
+        blog_content += f"{commit_message}\n\n"
     
     return blog_content
 
@@ -24,27 +24,12 @@ def main():
     # Generate blog post content from commit messages
     blog_content = generate_blog_post(repo_path)
 
-    # Generate the final HTML content
-    blog_html = f"""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>My Blog</title>
-    </head>
-    <body>
-        <h1>My Blog</h1>
-
-        <!-- Blog post content -->
-        {blog_content}
-        <!-- End of blog post content -->
-    </body>
-    </html>
-    """
+    # Convert the blog post content to HTML
+    html_content = markdown2.markdown(blog_content)
 
     # Save the HTML content to index.html file
     with open("index.html", "w", encoding="utf-8") as file:
-        file.write(blog_html)
+        file.write(html_content)
 
 if __name__ == "__main__":
     main()
